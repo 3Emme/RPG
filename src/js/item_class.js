2ad774d310fb4ff65c1bbaa0d141ff0127bea4f1
Ben Stoller <stollersystemsound@gmail.com>
2020-09-02 11:29:56 -0700

    build item_class constructor and pass test 1
    co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

---
+++ src/js/item_class.js
@@ -0,0 +1,12 @@
+export default class Item {
+  constructor(name,Id,worth,Hp,level,status,flags,rarity) {
+    this.name = name
+    this.Id = Id
+    this.worth = worth
+    this.Hp = Hp
+    this.level = level
+    this.status = status
+    this.flags = flags
+    this.rarity = rarity 
+  }
+}
