424a24cdf2b704663bb9323717211ca6b529dc80
Ben Stoller <stollersystemsound@gmail.com>
2020-09-02 11:00:17 -0700

    project start and build initial game class
    co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

---
+++ src/js/game_class.js
@@ -0,0 +1,11 @@
+export default class Game {
+  constructor(players,items,monsters,environments) {
+    this.players = players
+    this.items = items
+    this.monsters = monsters
+    this.environments = environments
+    this.gameClock = 0       
+  }
+};
+
+let RPG = new Game([],[],[],[])
