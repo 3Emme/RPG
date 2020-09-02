75dfa06deb319474d6d7df7042a2702e38db13c5
Ben Stoller <stollersystemsound@gmail.com>
2020-09-02 11:51:07 -0700

    build monster_class constructor and pass test 1
    co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

---
+++ __tests__/monster_class.test.js
@@ -0,0 +1,17 @@
+import Monster from '../src/js/monster_class.js';
+
+describe('Monster', () => {
+
+  test('should create an item object', () =>{
+    const monster = new Monster("test",1,1,1,[],[],1,[],"stats")
+    expect(monster.name).toEqual("test")
+    expect(monster.cr).toEqual(1)
+    expect(monster.hp).toEqual(1)
+    expect(monster.xp).toEqual(1)
+    expect(monster.inv).toEqual([])
+    expect(monster.equip).toEqual([])
+    expect(monster.mp).toEqual(1)
+    expect(monster.status).toEqual([]) 
+    expect(monster.stats).toEqual("stats")     
+  });
+})
---
+++ src/js/monster_class.js
@@ -0,0 +1,26 @@
+export default class Monster {
+  constructor(name,cr,hp,xp,inventory,equipment,mp,status,stats) {
+    this.name = name,
+    this.cr = cr,
+    this.hp = hp,
+    this.xp = xp,
+    this.inv = inventory,
+    this.equip = equipment,
+    this.mp = mp,
+    this.status = status    
+    this.stats = stats    
+  } 
+}
+
+class Stats {
+  constructor(str,dex,con,wis,int,chr,lck) {
+    this.str = str
+    this.dex = dex
+    this.con = con
+    this.wis = wis
+    this.int = int
+    this.chr = chr
+    this.lck = lck 
+
+  }
+}
