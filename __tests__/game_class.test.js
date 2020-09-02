1981332122aaa9b3d16ca256e6c587570cb6f7f0
Ben Stoller <stollersystemsound@gmail.com>
2020-09-02 11:03:06 -0700

    project start and build initial game class
    co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

---
+++ __tests__/game_class.test.js
@@ -0,0 +1,12 @@
+import Game from '../src/js/game_class.js';
+
+describe('Game', () => {
+
+  test('should create a game object', () =>{
+    const RPG = new Game([],[],[],[])
+    expect(RPG.players).toEqual([])
+    expect(RPG.items).toEqual([])
+    expect(RPG.monsters).toEqual([])
+    expect(RPG.environments).toEqual([])
+  });
+})
