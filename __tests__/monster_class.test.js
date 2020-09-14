c4efa1af7beb57738b1c19d1562241b016ed23ef
3Emme <emmettbuentiempo@gmail.com>
2020-09-13 20:52:18 -0700

    refactor tests to match new Monster class structure, and use a beforeEach instance of the monster variable

--- __tests__/monster_class.test.js
+++ __tests__/monster_class.test.js
@@ -1,17 +1,25 @@
 import Monster from '../src/js/monster_class.js';
+import { AbilityScores } from '../src/js/character_class.js';
+
 
 describe('Monster', () => {
-
-  test('should create an item object', () =>{
-    const monster = new Monster("test",1,1,1,[],[],1,[],"stats")
-    expect(monster.name).toEqual("test")
-    expect(monster.cr).toEqual(1)
-    expect(monster.hp).toEqual(1)
-    expect(monster.xp).toEqual(1)
-    expect(monster.inv).toEqual([])
-    expect(monster.equip).toEqual([])
-    expect(monster.mp).toEqual(1)
-    expect(monster.status).toEqual([]) 
-    expect(monster.abilityScores).toEqual("stats")     
+  let monster;
+  beforeEach(() => {
+    let abilityScores = new AbilityScores(1,1,1,1,1,1,1)
+    monster = new Monster(1,1,abilityScores,1,1,1,1,1,1);
+  });
+  
+  test('should create an monster object', () =>{    
+    expect(monster.id).toEqual(1);
+    expect(monster.name).toEqual(1);
+    expect(monster.abilityScores.dex).toEqual(1);
+    expect(monster.type.main).toEqual(1);
+    expect(monster.cr).toEqual(1);
+    expect(monster.hp).toEqual(1);
+    expect(monster.mp).toEqual(1);
+    expect(monster.status.blinded).toEqual(false);
+    expect(monster.inv).toEqual(1);
+    expect(monster.equip.head).toEqual([]);    
+    expect(monster.behaviors).toEqual(1);   
   });
 })
